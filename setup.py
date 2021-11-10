bd76e10e3ccf75a1881d722331f63af6cca9758e
Alexander Rodionov <alx1dr@gmail.com>
2021-11-10 20:29:19 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='force_absolute_imports',
+    version='0.1',
+    description='tiny tool to force absolute imports in python code',
+    long_description_content_type="text/markdown",
+    url='https://github.com/tandav/force_absolute_imports',
+    packages=find_packages(),
+    include_package_data=True,
+)
